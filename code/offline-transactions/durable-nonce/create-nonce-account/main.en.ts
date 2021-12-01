c22b9e8613579f6b0fe1404e77a4a30aef0b0562
yihau <a122092487@gmail.com>
2021-12-02 00:15:34 +0800

    feat: create a random nonce account auth

--- code/offline-transactions/durable-nonce/create-nonce-account/main.en.ts
+++ code/offline-transactions/durable-nonce/create-nonce-account/main.en.ts
@@ -7,7 +7,6 @@ import {
   SystemProgram,
   LAMPORTS_PER_SOL,
 } from "@solana/web3.js";
-import * as bs58 from "bs58";
 
 (async () => {
   // Setup our connection and wallet
@@ -18,10 +17,7 @@ import * as bs58 from "bs58";
   const airdropSignature = await connection.requestAirdrop(feePayer.publicKey, LAMPORTS_PER_SOL);
   await connection.confirmTransaction(airdropSignature);
 
-  // G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
-  const alice = Keypair.fromSecretKey(
-    bs58.decode("4NMwxzmYj2uvHuq8xoqhY8RXg63KSVJM1DXkpbmkUY7YQWuoyQgFnnzn6yo3CMnqZasnNPNuAT2TLwQsCaKkUddp")
-  );
+  const nonceAccountAuth = Keypair.generate();
 
   let nonceAccount = Keypair.generate();
   console.log(`nonce account: ${nonceAccount.publicKey.toBase58()}`);
@@ -38,7 +34,7 @@ import * as bs58 from "bs58";
     // init nonce account
     SystemProgram.nonceInitialize({
       noncePubkey: nonceAccount.publicKey, // nonce account pubkey
-      authorizedPubkey: alice.publicKey, // nonce account authority (for advance and close)
+      authorizedPubkey: nonceAccountAuth.publicKey, // nonce account authority (for advance and close)
     })
   );
 
