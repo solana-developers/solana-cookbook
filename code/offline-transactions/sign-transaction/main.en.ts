5c6ecd697fd51edfbd40116cd0762514da9860fc
Yihau Chen <a122092487@gmail.com>
2021-12-03 06:41:01 +0800

    feat: seperate offline tx into four steps (#38)

--- code/offline-transactions/sign-transaction/main.en.ts
+++ code/offline-transactions/sign-transaction/main.en.ts
@@ -10,6 +10,12 @@ import {
 import * as nacl from "tweetnacl";
 import * as bs58 from "bs58";
 
+// to complete a offline transaction, I will seperate them into four steps
+// 1. Create Transaction
+// 2. Sign Transaction
+// 3. Recover Transaction
+// 4. Send Transaction
+
 (async () => {
   // create connection
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
@@ -22,6 +28,7 @@ import * as bs58 from "bs58";
   await connection.confirmTransaction(await connection.requestAirdrop(alice.publicKey, LAMPORTS_PER_SOL));
   const bob = Keypair.generate();
 
+  // 1. Create Transaction
   let tx = new Transaction().add(
     SystemProgram.transfer({
       fromPubkey: alice.publicKey,
@@ -31,31 +38,36 @@ import * as bs58 from "bs58";
   );
   tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
   tx.feePayer = feePayer.publicKey;
+  let realDataNeedToSign = tx.serializeMessage(); // the real data singer need to sign.
 
-  // the real data should be signed
-  let realDataNeedToSign = tx.serializeMessage();
-
+  // 2. Sign Transaction
   // use any lib you like, the main idea is to use ed25519 to sign it.
   // the return signature should be 64 bytes.
   let feePayerSignature = nacl.sign.detached(realDataNeedToSign, feePayer.secretKey);
   let aliceSignature = nacl.sign.detached(realDataNeedToSign, alice.secretKey);
 
+  // 3. Recover Tranasction
   // there are two ways you can recover the tx
-
-  // 1. use populate then addSignauture
+  // 3.a Recover Tranasction (use populate then addSignauture)
   {
     let recoverTx = Transaction.populate(Message.from(realDataNeedToSign));
     recoverTx.addSignature(feePayer.publicKey, Buffer.from(feePayerSignature));
     recoverTx.addSignature(alice.publicKey, Buffer.from(aliceSignature));
+
+    // 4. Send transaction
     console.log(`txhash: ${await connection.sendRawTransaction(recoverTx.serialize())}`);
   }
 
-  // 2. use populate with signature
+  // or
+
+  // 3.b. Recover Tranasction (use populate with signature)
   {
     let recoverTx = Transaction.populate(Message.from(realDataNeedToSign), [
       bs58.encode(feePayerSignature),
       bs58.encode(aliceSignature),
     ]);
+
+    // 4. Send transaction
     console.log(`txhash: ${await connection.sendRawTransaction(recoverTx.serialize())}`);
   }
 
