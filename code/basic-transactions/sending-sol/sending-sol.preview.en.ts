c7323fd0c94398a8b0ddf5a458a78a706717990a
Jacob CG <82475023+Jacob-CG@users.noreply.github.com>
2021-12-03 16:44:39 -0600

    feat(preview): Add preview to basic transactions

---
+++ code/basic-transactions/sending-sol/sending-sol.preview.en.ts
@@ -0,0 +1,8 @@
+const transferTransaction = new Transaction()
+  .add(SystemProgram.transfer({
+    fromPubkey: fromKeypair.publicKey,
+    toPubkey: toKeypair.publicKey,
+    lamports: lamportsToSend
+  }))
+
+await sendAndConfirmTransaction(connection, transferTransaction, [fromKeypair]);
