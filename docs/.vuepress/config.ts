e6f9cbc2d92ed2caf6e5977c45cfa163faa401b2
Sayantan Karmakar <sayantankarmakar@outlook.com>
2021-12-03 15:29:21 +0530

    create-nft-token [ts]

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -33,6 +33,7 @@ export default defineUserConfig<DefaultThemeOptions> ({
                 '/recipes/basic-transactions.md',
                 '/recipes/accounts.md',
                 '/recipes/token.md',
+                '/recipes/nft.md',
                 '/recipes/staking.md',
               ]
             }
