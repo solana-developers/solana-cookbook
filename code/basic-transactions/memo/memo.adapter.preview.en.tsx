612e9b73968af075b157a5517ab588d3a91eab39
Jacob CG <82475023+Jacob-CG@users.noreply.github.com>
2021-12-03 17:13:52 -0600

    feat(preview): Add previews to Basic Transactions (#58)
    * chore: update offline transaction copy
    
    * chore: update readme
    
    * feat{preview}: update local dev guide with previews
    
    * feat(preview): Update keypairs and wallets with previews
    
    * feat(preview): Add preview to basic transactions

---
+++ code/basic-transactions/memo/memo.adapter.preview.en.tsx
@@ -0,0 +1,19 @@
+const transaction = new Transaction().add(
+  SystemProgram.transfer({
+    fromPubkey: publicKey,
+    toPubkey: Keypair.generate().publicKey,
+    lamports: 10,
+  })
+);
+
+await transaction.add(
+  new TransactionInstruction({
+    keys: [{ pubkey: publicKey, isSigner: true, isWritable: true }],
+    data: Buffer.from('Data to send in transaction', 'utf-8'),
+    programId: new PublicKey("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"),
+  })
+)
+
+const signature = await sendTransaction(transaction, connection);
+
+await connection.confirmTransaction(signature, 'processed');
---
+++ code/basic-transactions/sending-spl-token/sending-spl-token.preview.en.ts
@@ -0,0 +1,18 @@
+// Add token transfer instructions to transaction
+const transaction = new web3.Transaction().add(
+  splToken.Token.createTransferInstruction(
+    splToken.TOKEN_PROGRAM_ID,
+    fromTokenAccount.address,
+    toTokenAccount.address,
+    fromWallet.publicKey,
+    [],
+    1,
+  ),
+);
+
+// Sign transaction, broadcast, and confirm
+await web3.sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [fromWallet]
+);
