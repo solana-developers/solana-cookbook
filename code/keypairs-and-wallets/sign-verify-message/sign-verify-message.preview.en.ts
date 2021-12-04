31d3dacc82e552ac906a786145a64e08ddbd317a
Friedrich Grabner <max@grabner.co.uk>
2021-12-04 11:42:41 +0000

    added instructions for signing and verifying messages using TweetNaCl lib

---
+++ code/keypairs-and-wallets/sign-verify-message/sign-verify-message.preview.en.ts
@@ -0,0 +1,17 @@
+const keypair = Keypair.fromSecretKey(
+Uint8Array.from([
+    174, 47, 154, 16, 202, 193, 206, 113, 199, 190, 53, 133, 169, 175, 31, 56, 222, 53, 138, 189, 224, 216, 117,
+    173, 10, 149, 53, 45, 73, 251, 237, 246, 15, 185, 186, 82, 177, 240, 148, 69, 241, 227, 167, 80, 141, 89, 240,
+    121, 121, 35, 172, 247, 68, 251, 226, 218, 48, 63, 176, 109, 168, 89, 238, 135,
+  ])
+);
+
+const message = "The quick brown fox jumps over the lazy dog";
+const messageBytes = decodeUTF8(message);
+
+const signature = nacl.sign.detached(messageBytes, keypair.secretKey);
+const result = nacl.sign.detached.verify(messageBytes, signature, keypair.publicKey.toBytes());
+
+console.log(result); 
+// true
+  
