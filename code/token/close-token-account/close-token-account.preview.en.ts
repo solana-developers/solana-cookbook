bb34e15942648958e043fcaabcaf9432cb0b0eb0
yihau <a122092487@gmail.com>
2021-12-09 01:22:18 +0800

    feat: update close token account

--- code/token/close-token-account/close-token-account.preview.en.ts
+++ code/token/close-token-account/close-token-account.preview.en.ts
@@ -1,16 +1,11 @@
-const feePayer = Keypair.fromSecretKey(bs58.decode(""));
-const tokenAccountPubkey = new PublicKey("");
-const tokenAccountOwner = Keypair.fromSecretKey(bs58.decode(""));
-const destinationPubkey = new PublicKey("");
-
 let tx = new Transaction().add(
   Token.createCloseAccountInstruction(
     TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
     tokenAccountPubkey, // token account which you want to close
-    destinationPubkey, // destination
-    tokenAccountOwner.publicKey, // owner of token account
+    alice.publicKey, // destination
+    alice.publicKey, // owner of token account
     [] // for multisig
   )
 );
 
-await connection.sendTransaction(tx, [feePayer, tokenAccountOwner]);
+console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, alice /* fee payer + owner */])}`);
