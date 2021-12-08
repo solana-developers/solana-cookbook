f422e8f6467bf5be8093baf5189945e9692e477c
yihau <a122092487@gmail.com>
2021-12-09 01:26:50 +0800

    feat: update get token balance

--- code/token/get-token-balance/get-token-balance.preview.en.ts
+++ code/token/get-token-balance/get-token-balance.preview.en.ts
@@ -1,5 +1,1 @@
-const tokenAccount = new PublicKey("FWZedVtyKQtP4CXhT7XDnLidRADrJknmZGA2qNjpTPg8");
-
 let tokenAmount = await connection.getTokenAccountBalance(tokenAccount);
-console.log(`amount: ${tokenAmount.value.amount}`);
-console.log(`decimals: ${tokenAmount.value.decimals}`);
