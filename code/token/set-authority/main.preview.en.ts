5f5ce1ddd0e1b1ada72f3abd17ce9402ba104579
yihau <a122092487@gmail.com>
2021-12-09 01:28:54 +0800

    feat: update set auth

--- code/token/set-authority/main.preview.en.ts
+++ code/token/set-authority/main.preview.en.ts
@@ -1,17 +1,12 @@
-const feePayer = Keypair.fromSecretKey(bs58.decode(""));
-const mint = new PublicKey("");
-const oriAuth = Keypair.fromSecretKey(bs58.decode(""));
-const newAuth = new PublicKey("");
-
 let tx = new Transaction().add(
   Token.createSetAuthorityInstruction(
     TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
-    mint, // mint acocunt || token account
-    newAuth, // new auth (you can pass `null` to close it)
+    mintPubkey, // mint acocunt || token account
+    feePayer.publicKey, // new auth (you can pass `null` to close it)
     "MintTokens", // authority type, there are 4 types => 'MintTokens' | 'FreezeAccount' | 'AccountOwner' | 'CloseAccount'
-    oriAuth.publicKey, // original auth
+    alice.publicKey, // original auth
     [] // for multisig
   )
 );
 
-await connection.sendTransaction(tx, [feePayer, oriAuth]);
+console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, alice /* fee payer + origin auth */])}`);
