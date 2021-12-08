655b2dc895f8934702959436240a2d9cca841dee
yihau <a122092487@gmail.com>
2021-12-09 01:26:23 +0800

    feat: update get token account

--- code/token/get-token-account-by-owner/by-mint.preview.en.ts
+++ code/token/get-token-account-by-owner/by-mint.preview.en.ts
@@ -1,13 +1,1 @@
-const owner = new PublicKey("");
-const mint = new PublicKey("");
-
 let response = await connection.getParsedTokenAccountsByOwner(owner, { mint: mint });
-
-response.value.forEach((accountInfo) => {
-  console.log(`pubkey: ${accountInfo.pubkey.toBase58()}`);
-  console.log(`mint: ${accountInfo.account.data["parsed"]["info"]["mint"]}`);
-  console.log(`owner: ${accountInfo.account.data["parsed"]["info"]["owner"]}`);
-  console.log(`decimals: ${accountInfo.account.data["parsed"]["info"]["tokenAmount"]["decimals"]}`);
-  console.log(`amount: ${accountInfo.account.data["parsed"]["info"]["tokenAmount"]["amount"]}`);
-  console.log("====================");
-});
