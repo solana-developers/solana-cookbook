80526aa77be7b2bc4c2cd941e37b2599080bbf67
yihau <a122092487@gmail.com>
2021-12-09 01:20:01 +0800

    feat: update approve

--- code/token/approve/main.preview.en.ts
+++ code/token/approve/main.preview.en.ts
@@ -1,18 +1,12 @@
-const feePayer = Keypair.fromSecretKey(bs58.decode(""));
-const tokenAccountPubkey = new PublicKey("");
-const tokenAccountOwner = Keypair.fromSecretKey(bs58.decode(""));
-const delegateToPubkey = new PublicKey("");
-const amount = 1;
-
 let tx = new Transaction().add(
   Token.createApproveInstruction(
     TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
     tokenAccountPubkey, // token account
-    delegateToPubkey, // delegate
-    tokenAccountOwner.publicKey, // original auth
+    feePayer.publicKey, // delegate
+    alice.publicKey, // original auth
     [], // for multisig
-    amount
+    1 // allowed amount
   )
 );
 
-await connection.sendTransaction(tx, [feePayer, tokenAccountOwner]);
+console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, alice /* fee payer + origin auth */])}`);
