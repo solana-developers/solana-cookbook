67079f36b9d79caf33682ea4b87ff9214626696a
yihau <a122092487@gmail.com>
2021-12-09 01:28:32 +0800

    feat: update revoke

--- code/token/revoke/main.preview.en.ts
+++ code/token/revoke/main.preview.en.ts
@@ -1,14 +1,10 @@
-const feePayer = Keypair.fromSecretKey(bs58.decode(""));
-const tokenAccount = new PublicKey("");
-const tokenAccountOwner = Keypair.fromSecretKey(bs58.decode(""));
-
 let tx = new Transaction().add(
   Token.createRevokeInstruction(
     TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
-    tokenAccount, // token account
-    tokenAccountOwner.publicKey, // original auth
+    tokenAccountPubkey, // token account
+    alice.publicKey, // original auth
     [] // for multisig
   )
 );
 
-await connection.sendTransaction(tx, [feePayer, tokenAccountOwner]);
+console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, alice /* fee payer + origin auth */])}`);
