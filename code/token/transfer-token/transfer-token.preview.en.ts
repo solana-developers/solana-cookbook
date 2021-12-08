a3ad3b08be5f539efc85112a66c66c3519826349
yihau <a122092487@gmail.com>
2021-12-09 01:29:12 +0800

    feat: update transfer token

--- code/token/transfer-token/transfer-token.preview.en.ts
+++ code/token/transfer-token/transfer-token.preview.en.ts
@@ -1,20 +1,14 @@
-const feePayer = Keypair.fromSecretKey(bs58.decode(""));
-const fromPukbey = new PublicKey("");
-const toPubkey = new PublicKey("");
-const mintPubkey = new PublicKey("");
-const ownerOfFrom = Keypair.fromSecretKey(bs58.decode(""));
-
 let tx = new Transaction().add(
   Token.createTransferCheckedInstruction(
     TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
-    fromPukbey, // from (should be a token account)
+    tokenAccountXPubkey, // from (should be a token account)
     mintPubkey, // mint
-    toPubkey, // to (should be a token account)
-    ownerOfFrom.publicKey, // owner of from
+    tokenAccountYPubkey, // to (should be a token account)
+    alice.publicKey, // owner of from
     [], // for multisig account, leave empty.
     1e8, // amount, if your deciamls is 8, send 10^8 for 1 token
     8 // decimals
   )
 );
 
-await connection.sendTransaction(tx, [feePayer, ownerOfFrom]);
+console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, alice /* fee payer + owner */])}`);
