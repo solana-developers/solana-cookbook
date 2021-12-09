ae90eaed44061079b5205fca055383ac13657527
J <jneeraja@iitg.ac.in>
2021-12-10 01:56:48 +0530

    feat(create PDA): fixing minor stuff

--- code/accounts/program-derived-address/create-a-pda/client/main.en.ts
+++ code/accounts/program-derived-address/create-a-pda/client/main.en.ts
@@ -10,53 +10,52 @@ import {
     SYSVAR_RENT_PUBKEY
   } from "@solana/web3.js";
   
-  (async () => {
-    // program id
-    const programId = new PublicKey("7ZP42kRwUQ2zgbqXoaXzAFaiQnDyp6swNktTSv8mNQGN");
-  
-    // connection
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-  
-    // setup fee payer
-    const feePayer = Keypair.generate();
-    const feePayerAirdropSignature = await connection.requestAirdrop(feePayer.publicKey, LAMPORTS_PER_SOL);
-    await connection.confirmTransaction(feePayerAirdropSignature);
-  
-    // setup pda
-    let [pda, bump] = await PublicKey.findProgramAddress([feePayer.publicKey.toBuffer()], programId);
-    console.log(`bump: ${bump}, pubkey: ${pda.toBase58()}`);
+(async () => {
+  // program id
+  const programId = new PublicKey("7ZP42kRwUQ2zgbqXoaXzAFaiQnDyp6swNktTSv8mNQGN");
 
-    const data_size = 0;
-  
-    let tx = new Transaction().add(
-      new TransactionInstruction({
-        keys: [
-          {
-            pubkey: feePayer.publicKey,
-            isSigner: true,
-            isWritable: true,
-          },
-          {
-            pubkey: pda,
-            isSigner: false,
-            isWritable: true,
-          },
-          {
-            pubkey: SYSVAR_RENT_PUBKEY,
-            isSigner: false,
-            isWritable: false,
-          },
-          {
-            pubkey: SystemProgram.programId,
-            isSigner: false,
-            isWritable: false,
-          }
-        ],
-        data: Buffer.from(new Uint8Array([data_size, bump])),
-        programId: programId,
-      })
-    );
-  
-    console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer])}`);
-  })();
-  
+  // connection
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+  // setup fee payer
+  const feePayer = Keypair.generate();
+  const feePayerAirdropSignature = await connection.requestAirdrop(feePayer.publicKey, LAMPORTS_PER_SOL);
+  await connection.confirmTransaction(feePayerAirdropSignature);
+
+  // setup pda
+  let [pda, bump] = await PublicKey.findProgramAddress([feePayer.publicKey.toBuffer()], programId);
+  console.log(`bump: ${bump}, pubkey: ${pda.toBase58()}`);
+
+  const data_size = 0;
+
+  let tx = new Transaction().add(
+    new TransactionInstruction({
+      keys: [
+        {
+          pubkey: feePayer.publicKey,
+          isSigner: true,
+          isWritable: true,
+        },
+        {
+          pubkey: pda,
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: SYSVAR_RENT_PUBKEY,
+          isSigner: false,
+          isWritable: false,
+        },
+        {
+          pubkey: SystemProgram.programId,
+          isSigner: false,
+          isWritable: false,
+        }
+      ],
+      data: Buffer.from(new Uint8Array([data_size, bump])),
+      programId: programId,
+    })
+  );
+
+  console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer])}`);
+})();
