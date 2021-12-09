caf64800f742105077791aee08df46905d501b7b
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-12-10 03:18:55 +0530

    chore: more reduction of preview code

--- code/programs/cpi-transfer/program/src/lib.preview.rs
+++ code/programs/cpi-transfer/program/src/lib.preview.rs
@@ -1,49 +1,28 @@
-pub fn process_instruction(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    let accounts_iter = &mut accounts.iter();
+let token_transfer_amount = instruction_data
+    .get(..8)
+    .and_then(|slice| slice.try_into().ok())
+    .map(u64::from_le_bytes)
+    .ok_or(ProgramError::InvalidAccountData)?;
 
-    let source_token_account = next_account_info(accounts_iter)?;
-    let destination_token_account = next_account_info(accounts_iter)?;
-    let source_token_account_holder = next_account_info(accounts_iter)?;
-    let token_program = next_account_info(accounts_iter)?;
+let transfer_tokens_instruction = transfer(
+    &token_program.key,
+    &source_token_account.key,
+    &destination_token_account.key,
+    &source_token_account_holder.key,
+    &[&source_token_account_holder.key],
+    token_transfer_amount,
+)?;
 
-    let token_transfer_amount = instruction_data
-        .get(..8)
-        .and_then(|slice| slice.try_into().ok())
-        .map(u64::from_le_bytes)
-        .ok_or(ProgramError::InvalidAccountData)?;
+let required_accounts_for_transfer = [
+    source_token_account.clone(),
+    destination_token_account.clone(),
+    source_token_account_holder.clone(),
+];
 
-    msg!(
-        "Transferring {} tokens from {} to {}",
-        token_transfer_amount,
-        source_token_account.key.to_string(),
-        destination_token_account.key.to_string()
-    );
+invoke(
+    &transfer_tokens_instruction,
+    &required_accounts_for_transfer,
+)?;
 
-    let transfer_tokens_instruction = transfer(
-        &token_program.key,
-        &source_token_account.key,
-        &destination_token_account.key,
-        &source_token_account_holder.key,
-        &[&source_token_account_holder.key],
-        token_transfer_amount,
-    )?;
+Ok(())
 
-    let required_accounts_for_transfer = [
-        source_token_account.clone(),
-        destination_token_account.clone(),
-        source_token_account_holder.clone(),
-    ];
-
-    invoke(
-        &transfer_tokens_instruction,
-        &required_accounts_for_transfer,
-    )?;
-
-    msg!("Transfer successful :)");
-
-    Ok(())
-}
--- code/programs/get-clock/method-one/program/src/lib.preview.rs
+++ code/programs/get-clock/method-one/program/src/lib.preview.rs
@@ -1,21 +1,2 @@
-pub fn process_instruction(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    _instruction_data: &[u8],
-) -> ProgramResult {
-    let accounts_iter = &mut accounts.iter();
-    // Payer account
-    let payer_account = next_account_info(accounts_iter)?;
-    // Hello state account
-    // let some_account = ....
-
-    // Clock sysvar
-    let sysvar_clock_pubkey = next_account_info(accounts_iter)?;
-
-    // Getting Clock's time stamp
-    let clock = Clock::from_account_info(&sysvar_clock_pubkey)?;
-    let current_timestamp = clock.unix_timestamp;
-    msg!("Current Timestamp: {}", current_timestamp);
-
-    Ok(())
-}
+let clock = Clock::from_account_info(&sysvar_clock_pubkey)?;
+let current_timestamp = clock.unix_timestamp;
