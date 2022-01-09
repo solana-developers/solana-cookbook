fbc6f56bed22947a08f0eb7a00c39ca8d279d44c
Jacob Creech <jacob.creech@solana.com>
2022-01-08 22:41:24 -0600

    fix: update get NFT owner
    fixes #181

--- code/nfts/get-owner/get-owner.en.ts
+++ code/nfts/get-owner/get-owner.en.ts
@@ -6,7 +6,7 @@ import {Connection, PublicKey} from '@solana/web3.js';
 
     const largestAccounts = await connection.getTokenLargestAccounts(new PublicKey(tokenMint));
     const largestAccountInfo = await connection.getParsedAccountInfo(largestAccounts.value[0].address);
-    console.log(largestAccountInfo.value.owner);
+    console.log(largestAccountInfo.value.data.parsed.info.owner);
     /*
     PublicKey {
         _bn: <BN: 6ddf6e1d765a193d9cbe146ceeb79ac1cb485ed5f5b37913a8cf5857eff00a9>
