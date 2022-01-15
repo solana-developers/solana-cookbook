284d3a6db01ad084c883aa663d51140f4225d49b
losman0s <losmanos168@gmail.com>
2022-01-15 21:35:57 +0100

    Add reference for using mainnet accounts on localnet

---
+++ code/local-development/using-mainnet-accounts/dump-programs.en.sh
@@ -0,0 +1,2 @@
+# solana program dump -u <source cluster> <address of account to fetch> <destination file name/path>
+solana program dump -u m 9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin serum_dex_v3.so
---
+++ code/local-development/using-mainnet-accounts/load-accounts.preview.en.sh
@@ -0,0 +1,1 @@
+solana-test-validator --account SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt SRM_token.json --reset
