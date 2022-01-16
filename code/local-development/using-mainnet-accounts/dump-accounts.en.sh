c370d9309e1a399042bec9d6e3339d2ab25f2c81
man0s <95379755+losman0s@users.noreply.github.com>
2022-01-16 20:38:13 +0100

    feat(local-development): Add reference for using mainnet accounts on localnet (#205)

---
+++ code/local-development/using-mainnet-accounts/dump-accounts.en.sh
@@ -0,0 +1,2 @@
+# solana account -u <source cluster> --output <output format> --output-file <destination file name/path> <address of account to fetch>
+solana account -u m --output json-compact --output-file SRM_token.json SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt
---
+++ code/local-development/using-mainnet-accounts/load-accounts.en.sh
@@ -0,0 +1,2 @@
+# solana-test-validator --account <address to load the account to> <path to account file> --reset
+solana-test-validator --account SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt SRM_token.json --reset
