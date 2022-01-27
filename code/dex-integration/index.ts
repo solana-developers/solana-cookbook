4507b905b8c67f96c0555d9aa28eaa59c3f131d6
harsh4786 <50767810+harsh4786@users.noreply.github.com>
2022-01-27 17:56:35 +0530

    Update index.ts

--- code/dex-integration/index.ts
+++ code/dex-integration/index.ts
@@ -1,1 +1,7 @@
-
+import {Market, DexInstructions} from "@project-serum/serum";
+import {
+  SYSVAR_RENT_PUBKEY,
+  TransactionInstruction,
+  PublicKey,
+} from '@solana/web3.js';
+import BN from 'bn.js';
