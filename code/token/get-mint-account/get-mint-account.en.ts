dbccd286c2f51d9043368a76b482928f6b0412a1
yihau <a122092487@gmail.com>
2022-02-22 01:15:53 +0800

    feat: update get token mint code

--- code/token/get-mint-account/get-mint-account.en.ts
+++ code/token/get-mint-account/get-mint-account.en.ts
@@ -1,39 +1,28 @@
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
-import { MintLayout, u64 } from "@solana/spl-token";
+import { getMint } from "@solana/spl-token";
 
 (async () => {
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-  const mint = new PublicKey("54dQ8cfHsW1YfKYpmdVZhWpb9iSi6Pac82Nf7sg3bVb");
+  const mint = new PublicKey("8mAKLjGGmjKTnmcXeyr3pr7iX13xXVjJJiL6RujDbSPV");
 
-  // 1. use getParsedAccountInfo
+  let mintAccount = await getMint(connection, mint);
+
+  console.log(mintAccount);
+  /*
   {
-    let accountInfo = await connection.getParsedAccountInfo(mint);
-    // console.log(`raw data: ${JSON.stringify(accountInfo.value.data["parsed"]["info"])}`);
-    console.log(`supply: ${accountInfo.value.data["parsed"]["info"]["supply"]}`);
-    console.log(`decimals: ${accountInfo.value.data["parsed"]["info"]["decimals"]}`);
-    console.log(`mintAuthority: ${accountInfo.value.data["parsed"]["info"]["mintAuthority"]}`);
-    console.log(`freezeAuthority: ${accountInfo.value.data["parsed"]["info"]["freezeAuthority"]}`);
-  }
-
-  console.log("=====");
-
-  // 2. use getAccountInfo then deserialize data
-  {
-    let accountInfo = await connection.getAccountInfo(mint);
-    let mintInfo = MintLayout.decode(accountInfo.data);
-    // console.log(`raw data: ${mintInfo}`);
-    console.log(`supply: ${u64.fromBuffer(mintInfo.supply)}`);
-    console.log(`decimals: ${mintInfo.decimals}`);
-    if (mintInfo.mintAuthorityOption === 1) {
-      console.log(`mintAuthority: ${new PublicKey(mintInfo.mintAuthority)}`);
-    } else {
-      console.log(`mintAuthority: null`);
-    }
-    if (mintInfo.freezeAuthorityOption === 1) {
-      console.log(`freezeAuthority: ${new PublicKey(mintInfo.freezeAuthority)}`);
-    } else {
-      console.log(`freezeAuthority: null`);
+    address: PublicKey {
+      _bn: <BN: 7351e5e067cc7cfefef42e78915d3c513edbb8adeeab4d9092e814fe68c39fec>
+    },
+    mintAuthority: PublicKey {
+      _bn: <BN: df30e6ca0981c1a677eed6f7cb46b2aa442ca9b7a10a10e494badea4b9b6944f>
+    },
+    supply: 0n,
+    decimals: 8,
+    isInitialized: true,
+    freezeAuthority: PublicKey {
+      _bn: <BN: df30e6ca0981c1a677eed6f7cb46b2aa442ca9b7a10a10e494badea4b9b6944f>
     }
   }
+  */
 })();
