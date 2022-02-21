f2af654c8537a6e312d523c34480df913581077d
yihau <a122092487@gmail.com>
2022-02-22 02:08:35 +0800

    feat: update transfer token

--- code/token/transfer-token/transfer-token.preview.en.ts
+++ code/token/transfer-token/transfer-token.preview.en.ts
@@ -1,14 +1,29 @@
-let tx = new Transaction().add(
-  Token.createTransferCheckedInstruction(
-    TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
+// 1) use build-in function
+{
+  let txhash = await transferChecked(
+    connection, // connection
+    feePayer, // payer
     tokenAccountXPubkey, // from (should be a token account)
     mintPubkey, // mint
     tokenAccountYPubkey, // to (should be a token account)
-    alice.publicKey, // owner of from
-    [], // for multisig account, leave empty.
+    alice, // from's owner
     1e8, // amount, if your deciamls is 8, send 10^8 for 1 token
     8 // decimals
-  )
-);
+  );
+}
 
-console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, alice /* fee payer + owner */])}`);
+// or
+
+// 2) compose by yourself
+{
+  let tx = new Transaction().add(
+    createTransferCheckedInstruction(
+      tokenAccountXPubkey, // from (should be a token account)
+      mintPubkey, // mint
+      tokenAccountYPubkey, // to (should be a token account)
+      alice.publicKey, // from's owner
+      1e8, // amount, if your deciamls is 8, send 10^8 for 1 token
+      8 // decimals
+    )
+  );
+}
