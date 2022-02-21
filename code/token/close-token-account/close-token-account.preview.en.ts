eb51853953031cac41f8d566724f204780045f62
yihau <a122092487@gmail.com>
2022-02-22 02:19:11 +0800

    feat: update close token account

--- code/token/close-token-account/close-token-account.preview.en.ts
+++ code/token/close-token-account/close-token-account.preview.en.ts
@@ -1,11 +1,23 @@
-let tx = new Transaction().add(
-  Token.createCloseAccountInstruction(
-    TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
+// 1) use build-in function
+{
+  let txhash = await closeAccount(
+    connection, // connection
+    feePayer, // payer
     tokenAccountPubkey, // token account which you want to close
     alice.publicKey, // destination
-    alice.publicKey, // owner of token account
-    [] // for multisig
-  )
-);
+    alice // owner of token account
+  );
+}
 
-console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer, alice /* fee payer + owner */])}`);
+// or
+
+// 2) compose by yourself
+{
+  let tx = new Transaction().add(
+    createCloseAccountInstruction(
+      tokenAccountPubkey, // token account which you want to close
+      alice.publicKey, // destination
+      alice.publicKey // owner of token account
+    )
+  );
+}
