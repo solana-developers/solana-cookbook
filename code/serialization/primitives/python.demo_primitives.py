15a307f6ccac0c816b110eb23762ae90519ba65b
Kevin Heavey <kevinheavey123@gmail.com>
2022-02-22 01:43:07 +0000

    snake case

--- code/serialization/primitives/python.demo_primitives.py
+++ code/serialization/primitives/python.demo_primitives.py
@@ -2,7 +2,7 @@ import base64
 from borsh_construct import U8, U16, U32, String, HashMap
 
 # Schema to deserialize various types
-primitiveSchema = CStruct(
+primitive_schema = CStruct(
         "U8" / U8,
         "U16" / U16,
         "U32" / U32,
@@ -16,7 +16,7 @@ def common():
     mapping = {"cookbook": "recipe", "recipe": "ingredient"}
 
     # Serialize
-    dser = primitiveSchema.build({
+    dser = primitive_schema.build({
         'U8': 255,
         'U16': 65535,
         'U32': 4294967295,
@@ -26,7 +26,7 @@ def common():
     print(dser)
     # => b'\xff\xff\xff\xff\xff\xff\xff\x05\x00\x00\x00hello\x05\x00\x00\x00world\x01\x02\x03\x04\x05\x02\x00\x00\x00\x08\x00\x00\x00cookbook\x06\x00\x00\x00recipe\x06\x00\x00\x00recipe\n\x00\x00\x00ingredient'
     # Deserialize
-    new_value = primitiveSchema.parse(dser)
+    new_value = primitive_schema.parse(dser)
     # Viola
     print(new_value)
     # => Container:
