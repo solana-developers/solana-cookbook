08c0b8f53dbaee268660f16f64a5c4cf8010615a
Kevin Heavey <kevinheavey123@gmail.com>
2022-02-22 01:27:33 +0000

    fix schema and autoformat

--- code/serialization/instruction/python.client.py
+++ code/serialization/instruction/python.client.py
@@ -9,36 +9,32 @@ from solana.rpc.api import Client
 
 # Instruction variants for target program
 class InstructionVariant(IntEnum):
-    INITIALIZE = 0,
-    MINT = 1,
-    TRANSFER = 2,
+    INITIALIZE = (0,)
+    MINT = (1,)
+    TRANSFER = (2,)
     BURN = 3
 
 
 # Schema for sending instructionVariants to on-chain sample program
-payload_schema = CStruct(
-    "initialized" / U8,
-    "key" / String,
-    "value" / String
-)
+payload_schema = CStruct("id" / U8, "key" / String, "value" / String)
 
 
 def construct_payload(instructionVariant: InstructionVariant, key: str, value: str):
     """Generate a serialized instructionVariant"""
-    return payload_schema.build({'instructionVariant_id': instructionVariant, 'key': key, 'value': value})
+    return payload_schema.build({"id": instructionVariant, "key": key, "value": value})
 
 
 def mint_kv(
-        client: Client,
-        program_pk: PublicKey,
-        account_pk: PublicKey,
-        wallet_kp: Keypair,
-        mintKey: str,
-        mintValue: str) -> RPCResponse:
+    client: Client,
+    program_pk: PublicKey,
+    account_pk: PublicKey,
+    wallet_kp: Keypair,
+    mintKey: str,
+    mintValue: str,
+) -> RPCResponse:
     """Mint with a key/value pair to an account"""
     # Construct the program payload for Mint invariant
-    payload_ser = construct_payload(
-        InstructionVariant.MINT, mintKey, mintValue)
+    payload_ser = construct_payload(InstructionVariant.MINT, mintKey, mintValue)
 
     # print(payload_ser)
     # => b'\x01\n\x00\x00\x00python key\x0c\x00\x00\x00python value'
@@ -50,9 +46,10 @@ def mint_kv(
     # =>     value = u'python value' (total 12)
 
     # Construct the transaction with instructionVariant
-    txn = Transaction().add(TransactionInstruction(
-        [AccountMeta(account_pk, False, True)],
-        program_pk,
-        payload_ser))
+    txn = Transaction().add(
+        TransactionInstruction(
+            [AccountMeta(account_pk, False, True)], program_pk, payload_ser
+        )
+    )
     return client.send_transaction(txn, wallet_kp)
     # => {'jsonrpc': '2.0', 'result': '4ZdpWNdovdVaLextWSiqEBWp67k9rNTTUaX3qviHDXWY9c98bVtaRt5sasPhYzMVXHqhex78gzNKytcBnVH5CSTZ', 'id': 2}
