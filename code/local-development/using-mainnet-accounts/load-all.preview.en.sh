35764eed51ff4d0e176cef83b7ab83e06162edc3
Jacob Creech <jacob.creech@solana.com>
2022-02-26 21:54:34 -0600

    feat(seo): Update local development

--- code/local-development/using-mainnet-accounts/load-all.preview.en.sh
+++
@@ -1,4 +0,0 @@
-solana-test-validator \
-    --bpf-program 9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin serum_dex_v3.so \
-    --account SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt SRM_token.json \
-    --reset
