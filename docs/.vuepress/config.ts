860e0718bb62d5540e64963d7d9fa2f3d8bc111e
Jacob Creech <jacob.creech@solana.com>
2022-03-03 20:18:41 -0600

    feat(integrations): add integrations page

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -20,53 +20,67 @@ export default defineUserConfig<DefaultThemeOptions>({
             text: 'Contribute',
             link: 'https://github.com/solana-dev-adv/solana-cookbook',
           },
-        ],
-        sidebar: [
           {
-            text: 'Getting Started',
-            children: [
-              '/',
-              '/getting-started/installation.md'
-            ],
-          },
-          {
-            text: 'Core Concepts',
-            children: [
-              '/core-concepts/accounts.md',
-              '/core-concepts/programs.md',
-              '/core-concepts/transactions.md',
-              '/core-concepts/pdas.md',
-            ]
-          },
-          {
-            text: 'Guides',
-            children: [
-              '/guides/get-program-accounts.md',
-              '/guides/serialization.md',
-              '/guides/data-migration.md',
-              '/guides/account-maps.md',
-              '/guides/retrying-transactions.md',
-              '/guides/debugging-solana-programs.md',
-              '/guides/feature-parity-testing.md',
-            ],
-          },
-          {
-            text: 'References',
-            children: [
-              '/references/local-development.md',
-              '/references/keypairs-and-wallets.md',
-              '/references/basic-transactions.md',
-              '/references/accounts.md',
-              '/references/programs.md',
-              '/references/token.md',
-              '/references/anchor.md',
-              '/references/staking.md',
-              '/references/nfts.md',
-              '/references/offline-transactions.md',
-              '/references/name-service.md',
-            ],
+            text: 'Integrations',
+            link: '/integrations',
           },
         ],
+        sidebar: {
+          '/': [
+            {
+              text: 'Getting Started',
+              children: [
+                '/',
+                '/getting-started/installation.md'
+              ],
+            },
+            {
+              text: 'Core Concepts',
+              children: [
+                '/core-concepts/accounts.md',
+                '/core-concepts/programs.md',
+                '/core-concepts/transactions.md',
+                '/core-concepts/pdas.md',
+              ]
+            },
+            {
+              text: 'Guides',
+              children: [
+                '/guides/get-program-accounts.md',
+                '/guides/serialization.md',
+                '/guides/data-migration.md',
+                '/guides/account-maps.md',
+                '/guides/retrying-transactions.md',
+                '/guides/debugging-solana-programs.md',
+                '/guides/feature-parity-testing.md',
+              ],
+            },
+            {
+              text: 'References',
+              children: [
+                '/references/local-development.md',
+                '/references/keypairs-and-wallets.md',
+                '/references/basic-transactions.md',
+                '/references/accounts.md',
+                '/references/programs.md',
+                '/references/token.md',
+                '/references/anchor.md',
+                '/references/staking.md',
+                '/references/nfts.md',
+                '/references/offline-transactions.md',
+                '/references/name-service.md',
+              ],
+            },
+          ],
+          '/integrations': [
+            {
+              text: 'Integrations',
+              children: [
+                '/integrations',
+              ],
+            }
+          ]
+        },
       },
     },
   },
---
+++ docs/integrations/README.md
@@ -0,0 +1,20 @@
+# Getting Started with Integrations
+
+The Solana Cookbook integrations provides a set of examples and references for anyone
+to build on top of other protocols on Solana. 
+
+The power of Solana comes from the ability to integrate with other protocols,
+building on top of existing infrastructure to make something better.
+
+## Composability is Key
+
+Making your program easy to use and extend will grow both your program and the
+entire Solana ecosystem. Don't reinvent the wheel, build on top of existing protocols
+and bootstrap your userbase.
+
+Adapt and build on top of other programs or libraries and unlock new capabilities for Solana.
+
+## Contributing
+
+If you are interested in contributing to integrations, please checkout the [contribution
+guidelines](https://github.com/solana-dev-adv/solana-cookbook/blob/master/CONTRIBUTING.md).
