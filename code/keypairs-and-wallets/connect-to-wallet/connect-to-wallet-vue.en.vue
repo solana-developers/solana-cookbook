6cbede931d51e4d4913002a4fb858a60cbb6da23
stellaw1 <stella01wang@gmail.com>
2022-03-16 01:28:28 -0700

    feat: adds vue wallet adapter code snippet

---
+++ code/keypairs-and-wallets/connect-to-wallet/connect-to-wallet-vue.en.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { WalletMultiButton } from "solana-wallets-vue";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { initWallet, useWallet } from "solana-wallets-vue";
+
+const wallets = {
+  wallets: [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new TorusWalletAdapter(),
+    new LedgerWalletAdapter(),
+  ],
+};
+initWallet(wallets);
+
+const { connected, wallet } = useWallet();
+</script>
+
+<template>
+  <div>
+    <p v-if="connected">
+      Wallet with public key {{ wallet.publicKey }} successfully connected!
+    </p>
+    <div v-else>
+      <wallet-multi-button></wallet-multi-button>
+    </div>
+  </div>
+</template>
