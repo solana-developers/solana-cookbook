ed6c3fcf8228dff0275ce9d168afe55730e4a70b
dboures <deanboures@gmail.com>
2022-04-21 11:41:21 -0500

    feat: add user public key valid check (#325)
    * feat: add user public key valid check
    
    * fix: make Typescript check-valid-pub example immediately runnable

---
+++ code/keypairs-and-wallets/check-valid-publickey/check-valid-publickey.preview.py
@@ -0,0 +1,2 @@
+key = PublicKey('5oNDL3swdJJF1g9DzJiZ4ynHXgszjAEpUkxVYejchzrY')
+print(is_on_curve(key))
---
+++ code/keypairs-and-wallets/check-valid-publickey/check-valid-publickey.preview.ts
@@ -0,0 +1,2 @@
+const key = new PublicKey("5oNDL3swdJJF1g9DzJiZ4ynHXgszjAEpUkxVYejchzrY");
+console.log(PublicKey.isOnCurve(key.toBytes()));
