2d87ed86e5c33d28497739ec8c627568691f26b8
ProPratik1405 <sariapratik@gmail.com>
2022-05-19 22:39:56 +0545

    Messed up a ts file to test

--- code/account-maps/deriving-pda/client.preview.ts
+++ code/account-maps/deriving-pda/client.preview.ts
@@ -1,25 +1,25 @@
-(async () => {
-    const connection = new Connection("http://localhost:8899", "confirmed");
+async () => {
+  const connection = new Connection("http://localhost:8899", "confirmed");
 
-    const [blogAccount] = await PublicKey.findProgramAddress(
-        [Buffer.from("blog"), user.publicKey.toBuffer()],
-        MY_PROGRAM_ID
-    );
+  const [blogAccount] = await PublicKey.findProgramAddress(
+    [Buffer.from("blog"), user.publicKey.toBuffer()],
+    MY_PROGRAM_ID
+  );
 
-    const [postAccount] = await PublicKey.findProgramAddress(
-        [Buffer.from("post"), Buffer.from("slug-1"), user.publicKey.toBuffer()],
-        MY_PROGRAM_ID
-    );
+  const [postAccount] = await PublicKey.findProgramAddress(
+    [Buffer.from("post"), Buffer.from("slug-1"), user.publicKey.toBuffer()],
+    MY_PROGRAM_ID
+  );
 
-    const blogAccountInfo = await connection.getAccountInfo(blogAccount);
-    const blogAccountState = BLOG_ACCOUNT_DATA_LAYOUT.decode(
-        blogAccountInfo.data
-    );
-    console.log("Blog account state: ", blogAccountState);
+  const blogAccountInfo = await connection.getAccountInfo(blogAccount);
+  const blogAccountState = BLOG_ACCOUNT_DATA_LAYOUT.decode(
+    blogAccountInfo.data
+  );
+  console.log("Blog account state: ", blogAccountState);
 
-    const postAccountInfo = await connection.getAccountInfo(postAccount);
-    const postAccountState = POST_ACCOUNT_DATA_LAYOUT.decode(
-        postAccountInfo.data
-    );
-    console.log("Post account state: ", postAccountState);
-})
+  const postAccountInfo = await connection.getAccountInfo(postAccount);
+  const postAccountState = POST_ACCOUNT_DATA_LAYOUT.decode(
+    postAccountInfo.data
+  );
+  console.log("Post account state: ", postAccountState);
+};
