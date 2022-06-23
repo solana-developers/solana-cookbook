c975a238237792ca238e25836b16d313b61c089d
Kira272921 <sunset272921@gmail.com>
2022-06-23 09:51:48 +0530

    fix: update get all nfts from wallet to use new metaplex sdk

--- code/nfts/get-all-nfts/get-all-nfts.preview.en.ts
+++ code/nfts/get-all-nfts/get-all-nfts.preview.en.ts
@@ -1,4 +1,10 @@
-const connection = new Connection("mainnet-beta");
-const ownerPublickey = "OWNER_PUBLICK_KEY";
-const nftsmetadata = await Metadata.findDataByOwner(connection, ownerPublickey);
-console.log(nftsmetadata);
+const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+const keypair = Keypair.generate();
+
+const metaplex = new Metaplex(connection);
+metaplex.use(keypairIdentity(keypair));
+
+const owner = new PublicKey("2R4bHmSBHkHAskerTHE6GE1Fxbn31kaD5gHqpsPySVd7");
+const allNFTs = await metaplex.nfts().findAllByOwner(owner);
+
+console.log(allNFTs);
