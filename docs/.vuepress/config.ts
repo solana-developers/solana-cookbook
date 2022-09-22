1688889d1f8b9716b9000ce1343e411fc86b7672
TaeGit <tgsong827@gmail.com>
2022-09-23 00:12:30 +0900

    feat: Korean configs in config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -428,6 +428,79 @@ export default defineUserConfig<DefaultThemeOptions>({
           ],
         },
       },
+      "/kr/": {
+        selectLanguageName: "한국어",
+        navbar: [
+          {
+            text: "Contribute",
+            link: "https://github.com/solana-developers/solana-cookbook",
+          },
+          {
+            text: "Integrations",
+            link: "/integrations",
+          },
+        ],
+        sidebar: {
+          "/": [
+            {
+              text: "Getting Started",
+              children: ["/kr/", "/getting-started/installation.md"],
+            },
+            {
+              text: "Core Concepts",
+              children: [
+                "/core-concepts/accounts.md",
+                "/core-concepts/programs.md",
+                "/core-concepts/transactions.md",
+                "/core-concepts/pdas.md",
+              ],
+            },
+            {
+              text: "Guides",
+              children: [
+                "/guides/get-program-accounts.md",
+                "/guides/serialization.md",
+                "/guides/data-migration.md",
+                "/guides/account-maps.md",
+                "/guides/retrying-transactions.md",
+                "/guides/debugging-solana-programs.md",
+                "/guides/feature-parity-testing.md",
+              ],
+            },
+            {
+              text: "References",
+              children: [
+                "/references/local-development.md",
+                "/references/keypairs-and-wallets.md",
+                "/references/basic-transactions.md",
+                "/references/accounts.md",
+                "/references/programs.md",
+                "/references/token.md",
+                "/references/staking.md",
+                "/references/nfts.md",
+                "/references/offline-transactions.md",
+                "/references/name-service.md",
+              ],
+            },
+          ],
+          "/integrations": [
+            {
+              text: "Integrations",
+              children: [
+                "/integrations",
+                "/integrations/serum.md",
+                "/integrations/pyth.md",
+                "/integrations/switchboard.md",
+                "/integrations/mango.md",
+                "/integrations/strata.md",
+                "/integrations/web3auth.md",
+                "/integrations/react-native.md",
+                "/integrations/jupiter.md",
+              ],
+            },
+          ],
+        },
+      },
     },
   },
   locales: {
@@ -456,6 +529,10 @@ export default defineUserConfig<DefaultThemeOptions>({
     "/id/": {
       lang: "id-ID",
       title: "Buku Panduan Solana",
+    },
+    "/kr/": {
+      lang: "kr-KR",
+      title: "Solana Cookbook"
     },
   },
   markdown: {
@@ -489,6 +566,9 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           "/id/": {
             placeholder: "Cari",
+          },
+          "/kr/": {
+            placeholder: "검색",
           },
         },
         maxSuggestions: 10,
@@ -547,6 +627,14 @@ export default defineUserConfig<DefaultThemeOptions>({
             translations: {
               button: {
                 buttonText: "Cari",
+              },
+            },
+          },
+          "/kr/": {
+            placeholder: "검색",
+            translations: {
+              button: {
+                buttonText: "검색",
               },
             },
           },
