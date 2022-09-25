0c34b88e9ff96c663495d373d171d7c34d544cac
TaeGit <tgsong827@gmail.com>
2022-09-25 19:10:42 +0900

    docs(kr): Guides/Debugging Solana Programs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -463,7 +463,7 @@ export default defineUserConfig<DefaultThemeOptions>({
                 "/kr/guides/data-migration.md",
                 "/kr/guides/account-maps.md",
                 "/kr/guides/retrying-transactions.md",
-                "/guides/debugging-solana-programs.md",
+                "/kr/guides/debugging-solana-programs.md",
                 "/guides/feature-parity-testing.md",
               ],
             },
