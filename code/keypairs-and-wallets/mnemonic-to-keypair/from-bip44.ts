1a06168ebf84ece559b12bb5b36856f9a0ca948d
Bram Borggreve <beeman@beeman.nl>
2022-10-06 18:01:10 -0500

    refactor: use new, lightweight library for deriving keys from mnemonic

--- code/keypairs-and-wallets/mnemonic-to-keypair/from-bip44.ts
+++ code/keypairs-and-wallets/mnemonic-to-keypair/from-bip44.ts
@@ -1,16 +1,15 @@
 import { Keypair } from "@solana/web3.js";
-import { derivePath } from "ed25519-hd-key";
+import { HDKey } from "micro-ed25519-hdkey";
 import * as bip39 from "bip39";
 
 (async () => {
   const mnemonic =
     "neither lonely flavor argue grass remind eye tag avocado spot unusual intact";
   const seed = bip39.mnemonicToSeedSync(mnemonic, ""); // (mnemonic, password)
+  const hd = HDKey.fromMasterSeed(seed.toString("hex"));
   for (let i = 0; i < 10; i++) {
     const path = `m/44'/501'/${i}'/0'`;
-    const keypair = Keypair.fromSeed(
-      derivePath(path, seed.toString("hex")).key
-    );
+    const keypair = Keypair.fromSeed(hd.derive(path).privateKey);
     console.log(`${path} => ${keypair.publicKey.toBase58()}`);
   }
 })();
