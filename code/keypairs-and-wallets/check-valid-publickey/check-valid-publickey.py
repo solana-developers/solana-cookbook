17c9268feda44d1c465a6ecb35922339304447d6
Kevin Heavey <kevinheavey123@gmail.com>
2023-01-13 16:34:26 +0000

    fix is_on_curve

--- code/keypairs-and-wallets/check-valid-publickey/check-valid-publickey.py
+++ code/keypairs-and-wallets/check-valid-publickey/check-valid-publickey.py
@@ -1,9 +1,8 @@
 from solders.pubkey import Pubkey
-from solana.utils.ed25519_base import is_on_curve
 
 # Note that Keypair() will always give a public key that is valid for users
 key = Pubkey.from_string('5oNDL3swdJJF1g9DzJiZ4ynHXgszjAEpUkxVYejchzrY') # Valid public key
-print(is_on_curve(key)) # Lies on the ed25519 curve and is suitable for users
+print(key.is_on_curve()) # Lies on the ed25519 curve and is suitable for users
 
 off_curve_address = Pubkey.from_string('4BJXYkfvg37zEmBbsacZjeQDpTNx91KppxFJxRqrz48e') # Valid public key
 print(off_curve_address.is_on_curve()) # Not on the ed25519 curve, therefore not suitable for users
