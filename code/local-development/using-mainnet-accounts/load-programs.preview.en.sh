64384a6264c72093c3af6a04e2afd829cd23e930
Amin Memariani <amin90memar@gmail.com>
2024-04-13 18:16:11 +0330

    Update load-programs.preview.en.sh
    replaced serum with openbook

--- code/local-development/using-mainnet-accounts/load-programs.preview.en.sh
+++ code/local-development/using-mainnet-accounts/load-programs.preview.en.sh
@@ -1,1 +1,1 @@
-solana-test-validator --bpf-program 9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin serum_dex_v3.so --reset
+solana-test-validator --bpf-program 9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin openbook_dex.so --reset
